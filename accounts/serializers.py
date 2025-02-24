bf26b2a26e7dd407bd8d5c456d1188a1290f6377
artem <111177225+Artem-be@users.noreply.github.com>
2025-02-24 14:04:27 +0300

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -51,3 +51,20 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
             'access_token': str(token.access_token),
             'refresh_token': str(refresh_token.token),
         }
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'password']
+        extra_kwargs = {
+            'email': {'required': False},
+            'password': {'required': False},
+        }
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.password = validated_data.get('password', instance.password)
+        instance.save()
+        return instance
